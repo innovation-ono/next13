62fd43ea1c31e4ab1cb6ea740e8f8292be575bd7
大野厚 <a_ono@innovation.co.jp>
2024-11-07 22:20:38 +0900

    tailwindテスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <h2 className="text-red-500">Hello</h2>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
